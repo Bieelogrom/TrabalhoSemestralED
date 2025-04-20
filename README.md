d785cf40760729d67fba9d374d900bc4e9fcb79f
Bieelogrom <gabrielamorim15436@gmail.com>
2025-04-20 09:41:52 -0300

    feat: tela principal

---
+++ README.md
@@ -0,0 +1,8 @@
+# Trabalho Semestral - ED
+
+Trabalho semestral.
+**Disciplina :** Estrutura de Dados
+
+![enter image description here](https://bkpsitecpsnew.blob.core.windows.net/uploadsitecps/sites/137/2024/08/logo-fatec_zona_leste.png)
+
+ - ***Gabriel Ferreira Amorim  - ADS - Fatec Zona Leste / 2025***
---
+++ src/view/TelaPrincipal.java
@@ -0,0 +1,57 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+
+public class TelaPrincipal extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaPrincipal frame = new TelaPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaPrincipal() {
+		setTitle("Tela Principal");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPane.setBounds(12, 0, 426, 256);
+		contentPane.add(tabbedPane);
+		
+		JPanel panel = new JPanel();
+		tabbedPane.addTab("New tab", null, panel, null);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_1, null);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPane.addTab("New tab", null, panel_2, null);
+	}
+}
