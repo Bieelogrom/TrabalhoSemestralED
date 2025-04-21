ba9715f945de0f44fe30d8915e59c372d5307865
Bieelogrom <gabrielamorim15436@gmail.com>
2025-04-21 12:29:20 -0300

    fix: não tinha salvo alterações antes xD

--- src/model/Curso.java
+++ src/model/Curso.java
@@ -1,5 +1,39 @@
 package model;
 
 public class Curso {
+	private int codigoCurso;
+	private String nomeCurso;
+	private String areaConhecimento;
+	
+	public Curso(int codigoCurso, String nomeCurso, String areaConhecimento) {
+		this.codigoCurso = codigoCurso;
+		this.nomeCurso = nomeCurso;
+		this.areaConhecimento = areaConhecimento;
+	}
 
+	public int getCodigoCurso() {
+		return codigoCurso;
+	}
+
+	public void setCodigoCurso(int codigoCurso) {
+		this.codigoCurso = codigoCurso;
+	}
+
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+
+	public String getAreaConhecimento() {
+		return areaConhecimento;
+	}
+
+	public void setAreaConhecimento(String areaConhecimento) {
+		this.areaConhecimento = areaConhecimento;
+	}
+	
+	
 }
